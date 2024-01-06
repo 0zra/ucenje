fc6ab4ef8198442a3e827408cb3d8eefd6ac29fc
Petar Ozretic <petar.ozretic@locastic.com>
2024-01-06 13:59:14 +0100

    Ratelimit the endpoint

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -7,6 +7,23 @@ import { TRPCError } from "@trpc/server";
 
 const filterUsersForClient = (user: User) => ({id: user.id, username: user.username, profileImageUrl: user.profileImageUrl})
 
+
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
+
+// Create a new ratelimiter, that allows 3 requests per 1 min
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
+
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
@@ -39,6 +56,11 @@ export const postsRouter = createTRPCRouter({
     })
   ).mutation(async ({ ctx, input }) => {
     const authorId = ctx.userId;
+
+const {success} = await ratelimit.limit(authorId); // for deno: see above
+
+if(!success) throw new TRPCError({code: "TOO_MANY_REQUESTS", message: "You are doing that too much"})
+
     const post = await ctx.prisma.post.create({
       data: {
         content: input.content,
