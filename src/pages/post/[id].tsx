d54c99e6c53f13add0de4da5406a89c4c84d02ed
Petar Ozretic <petar.ozretic@locastic.com>
2024-01-08 14:44:20 +0100

    Add ci

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -2,24 +2,26 @@ import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { PageLayout } from "~/components/layout";
 import { api } from "~/utils/api";
-// import { PageLayout } from "~/components/layout";
-// import { PostView } from "~/components/postview";
+
+import { PostView } from "~/components/postview";
 // import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
-  // const { data } = api.posts.getById.useQuery({
-  // id,
-  // });
-  // if (!data) return <div>404</div>;
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+  if (!data) return <div>404</div>;
 
   return (
     <>
       <Head>
         <title>
-          Post {/*`${data.post.content} - @${data.author.username}`*/}
+          `${data.post.content} - @${data.author.username}`
         </title>
       </Head>
-      <PageLayout>{/* <PostView {...data} /> */}</PageLayout>
+      <PageLayout>
+        <PostView {...data} />{" "}
+      </PageLayout>
     </>
   );
 };
